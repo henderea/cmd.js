647c17d66c63d65ec9b1aef7223933b7a7fac8d2
Eric Henderson <henderea@gmail.com>
2018-11-05 15:56:22 -0500

    checkpoint - get initial usage, name, and action support

--- examples/init-func.js
+++ examples/init-func.js
@@ -2,6 +2,14 @@
 const command = require('..');
 
 command((cmd) => {
-    cmd.command('./subcommand');
-    console.log(cmd);
+    cmd.usage('<subcommand> [args...]');
+    let sc = cmd.command('./subcommand');
+    cmd.action(argv => {
+        console.log(argv);
+        console.log(cmd.fullName);
+        console.log(sc.fullName);
+        console.log(cmd.usageText);
+        console.log(sc.usageText);
+        console.log(cmd);
+    });
 }).parse();
--- examples/subcommand.js
+++ examples/subcommand.js
@@ -1,3 +1,3 @@
 const command = require('..');
 
-module.exports = command().name('subcommand');
+module.exports = command().name('subcommand', 'sc', 'scmd').usage('Usage: $0 <param> [param2...]').action(argv => { console.log(argv); });
