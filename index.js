a4f211aacef15ce3a67cb2b29c66513caa09e87f
Eric Henderson <henderea@gmail.com>
2018-12-28 17:56:07 -0500

    working on adding tests

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const path = require('path');
-const EventEmitter = require('events').EventEmitter;
-const requireDirectory = require('require-directory');
-const getCallerFile = require('get-caller-file');
-const util = require('./lib/util');
-const argsert = util.argsert;
-const _ = require('lodash');
 const Command = require('./lib/command');
 
 module.exports = (init) => new Command(init);
--- test/test.js
+++ test/test.js
@@ -1,11 +1,31 @@
-var assert = require('assert');
+var expect = require('expect.js');
 const command = require('..');
 const Command = command.Command;
 
 describe('Command', function() {
     describe('command()', function() {
         it('should return an instance of class Command', function() {
-            assert(command() instanceof Command);
+            expect(command()).to.be.a(Command);
+        });
+    });
+    describe('#parse', function() {
+        describe('#action', function() {
+            describe('argv', function() {
+                it('should set the argv properly', function() {
+                    let result = null;
+                    let argv = ['script', 'test', 'test2'];
+                    command().action(argv => result = argv).parse(argv);
+                    expect(result).to.eql(argv);
+                });
+            });
+            describe('#command', function() {
+                it('should take out the global parts of argv for sub-commands', function() {
+                    let result = null;
+                    let argv = ['script', 'command', 'test', 'test2'];
+                    command(cmd => cmd.command(command().name('command').action(argv => result = argv))).parse(argv);
+                    expect(result).to.eql(argv.slice(1));
+                });
+            });
         });
     });
 });
